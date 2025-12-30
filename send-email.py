e26422a109d5d2c79bc087928ceca437dca6aef9
akankshadeorankar <113322073+akankshadeorankar@users.noreply.github.com>
2025-12-30 12:25:46 +0530

    Update send-email.py

---
+++ send-email.py
@@ -0,0 +1,30 @@
+import smtplib
+
+from email.mime.text import MIMEText
+ 
+SMTP_SERVER = "mail.eidiko-india.com"
+
+SMTP_PORT = 465
+ 
+EMAIL_FROM = "akanksharajesh.deorankar@eidiko-india.com"
+
+EMAIL_TO = "shwetshailendra.gaikwad@eidiko-india.com"
+
+EMAIL_PASSWORD = "Deorankar@123"
+ 
+msg = MIMEText("This email was sent using GitHub Actions CI/CD.")
+
+msg['Subject'] = "CI/CD Email Test"
+
+msg['From'] = EMAIL_FROM
+
+msg['To'] = EMAIL_TO
+ 
+with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
+
+    server.login(EMAIL_FROM, EMAIL_PASSWORD)
+
+    server.send_message(msg)
+ 
+print("Email sent successfully")
+ 
